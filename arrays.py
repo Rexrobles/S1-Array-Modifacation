7922924a5c3f97898c1ce113224a87519b69f600
Rex-Imman Robles <reximman03@gmail.com>
2022-11-05 10:22:56 +0800

    Printing Arrays

---
+++ arrays.py
@@ -0,0 +1,14 @@
+# Write a python program that do the following:
+
+# - display the initial content of the array
+# - display a menu of options
+# - allow user to select item in the menu (check if valid)
+# - perform the selected option (you may prompt additional info to user when need)
+# - display the resulting values of the array
+
+
+# Note: 
+# - The program has an array variable containing 10 random numbers
+
+Arrays = [14, 3, 15, 27, 14, 10, 6, 24, 100, 5]
+print(Arrays)
